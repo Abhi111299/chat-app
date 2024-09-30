dc63546ef8666c564b0900b3c5f1f32f0bf80575
Abhishek Yadav <abhishek.yadav@debutinfotech.com>
2024-09-30 17:42:17 +0530

    chat module ui setup

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const http = require('http');
+const socketIo = require('socket.io'); 
 
 dotenv.config({path: "config/.env"});
 connectDB();
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+var usp = io.of('/user-namespace');
+
+usp.on('connection', function (socket) {
+    console.log("User connected");
+
+    socket.on('disconnect', function () {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(port, () => {
     console.log(`server is running on the port ${port}`);
 });
