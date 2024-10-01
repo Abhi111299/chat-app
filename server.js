2565800cadb4ebd806b66f4112e67f5a9b6bcef8
Abhishek Yadav <abhishek.yadav@debutinfotech.com>
2024-10-01 11:21:57 +0530

    online or offline status

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 const http = require('http');
-const socketIo = require('socket.io'); 
+const socketIo = require('socket.io');
+const User = require('./models/UserModel');
 
 dotenv.config({path: "config/.env"});
 connectDB();
@@ -15,11 +16,15 @@ const io = socketIo(server);
 
 var usp = io.of('/user-namespace');
 
-usp.on('connection', function (socket) {
+usp.on('connection',async function (socket) {
     console.log("User connected");
+    var user_id = socket.handshake.auth.token;
+    await User.findByIdAndUpdate({_id: user_id}, {$set:{ is_online: '1'}});
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect',async function () {
         console.log("User disconnected");
+        var user_id = socket.handshake.auth.token;
+        await User.findByIdAndUpdate({_id: user_id}, {$set:{ is_online: '0'}});
     });
 });
 
